feaeac6cc0029747c776012108eb67b1ff60e2a1
root <root@zee-portal.q5fraolp25ru3ntefb54wakvdh.rx.internal.cloudapp.net>
2023-09-08 08:33:49 +0000

    code refactor

--- models/locate.sql
+++ models/locate.sql
@@ -1,4 +1,4 @@
 -- to create a table instead of view, uncomment the line below
 --{{ config(materialized='table') }}
 
-select (_airbyte_data -> 'userLocationDetails' -> 'country')::varchar as country, (_airbyte_data -> 'userLocationDetails' -> 'city')::varchar as city from _airbyte_raw_users where _airbyte_data -> 'userLocationDetails' ? 'country'
+select (_airbyte_data -> 'userLocationDetails' ->> 'country')::varchar as country, (_airbyte_data -> 'userLocationDetails' ->> 'city')::varchar as city from _airbyte_raw_users where _airbyte_data -> 'userLocationDetails' ? 'country'
