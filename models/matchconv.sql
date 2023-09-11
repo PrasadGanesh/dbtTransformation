2da0125e2407af932e980ac32425c80bbf304423
Ganesh Prasad <ganeshprasad227@gmail.com>
2023-09-11 10:02:40 +0000

    fixed matchconv.sql

--- models/matchconv.sql
+++ models/matchconv.sql
@@ -1,4 +1,4 @@
 -- to create a table instead of view, uncomment the line below
 --{{ config(materialized='table') }}
 
-select _airbyte_data ->> 'matchStatus' as matchStatus, _airbyte_data ->> 'isConversationCreated' from _airbyte_raw_profilematchlogs where _airbyte_data -> 'matchStatus' = 1
+select (_airbyte_data ->> 'matchStatus')::INT as matchStatus, (_airbyte_data ->> 'isConversationCreated')::BOOLEAN as conversationStarted from _airbyte_raw_profilematchlogs where (_airbyte_data -> 'matchStatus')::INT = 1
