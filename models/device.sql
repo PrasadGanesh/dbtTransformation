d730e0a5a7ab8afd6762e4360ec832087a48960d
Ganesh Prasad <ganeshprasad227@gmail.com>
2023-09-12 12:11:14 +0000

    gave a heading to device type column

--- models/device.sql
+++ models/device.sql
@@ -1,3 +1,3 @@
 -- to create a table instead of view, uncomment the line below
 --{{ config(materialized='table') }}
-select _airbyte_data -> 'deviceDetail' ->> 'deviceTypeText' from _airbyte_raw_users
+select _airbyte_data -> 'deviceDetail' ->> 'deviceTypeText' as deviceType from _airbyte_raw_users
